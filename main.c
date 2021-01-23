fae3a79382739fc4ae60c823446a189f21e4008d
michaeltalla <mika-xuan@hottmail.com>
2021-01-23 14:00:17 +0100

    main et cmake

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
-//
 // Created by Nirina Gallot on 23/01/2021.
 //joined by Michael TALLA on 23/01/2021
 // Malloc-Fantasy
+#include <_stdio.h>
+#include <mysql.h>
+
+
+
+
 
 int main(int argc, char** argv){
-printf('avoue je suis bg');
-printf('non mais non ');
-
-printf(' et la ?');
+MYSQL *connexion = mysql_init(NULL);
+    if(connexion== NULL){
+        printf('failed');
+    }   else{
+        printf('success');
+    }
 }
