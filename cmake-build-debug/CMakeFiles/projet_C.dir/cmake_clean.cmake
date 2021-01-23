2408870ab8a597708bdde62e36542a9c58301008
Retr0 <ngallot@myges.fr>
2021-01-24 00:37:24 +0100

    Changes

---
+++ cmake-build-debug/CMakeFiles/projet_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../bin/projet_C.exe"
+  "../bin/projet_C.exe.manifest"
+  "../bin/projet_C.pdb"
+  "CMakeFiles/projet_C.dir/main.c.obj"
+  "libprojet_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
