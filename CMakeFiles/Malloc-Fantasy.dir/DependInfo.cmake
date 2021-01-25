9415c0dd3c7cb050f81ec5089260241733dac220
Retr0 <ngallot@myges.fr>
2021-01-25 15:32:06 +0100

    I Have no idea of what I am doing to do

---
+++ CMakeFiles/Malloc-Fantasy.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/Sources/draw.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/Sources/draw.c.obj"
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/Sources/init.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/Sources/init.c.obj"
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/Sources/input.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/Sources/input.c.obj"
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/Sources/map.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/Sources/map.c.obj"
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/Sources/sounds.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/Sources/sounds.c.obj"
+  "C:/Users/gallo_umchrss/Desktop/projets/projet C/main.c" "C:/Users/gallo_umchrss/Desktop/projets/projet C/CMakeFiles/Malloc-Fantasy.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "Mysql/include"
+  "C:/msys64/mingw64/bin/../include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
