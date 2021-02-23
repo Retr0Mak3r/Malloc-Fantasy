a0015249e6a4dbe1359a5115722a2a8e76de7011
mikalia <mika-xuan@hotmail.com>
2021-02-23 14:29:02 +0100

    version 2

--- Sources/draw.c
+++ Sources/draw.c
@@ -8,46 +8,15 @@ extern TTF_Font *police;
 
 
 void drawGame(state){
-    printf("draw game %d \n", state);
-    /*
-    //------------- couleur du renderer --------------
-    if(0 != SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-    }
-
-    if(0 != SDL_RenderClear(renderer))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-    }
-
-    SDL_RenderPresent(renderer);
-    SDL_Delay(500);
-    
-
-    if(0 != SDL_SetRenderDrawColor(renderer, 255, 127, 40, 255))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-    }
-
-    if(0 != SDL_RenderClear(renderer))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-    }
-    SDL_RenderPresent(renderer);
-    */
-
-    //surface = SDL_LoadBMP("../bin/graphics/title_2.bmp");
     if(state==1)
     {
-
+        // background
         image = SDL_LoadBMP("../bin/graphics/title_2.bmp");
         texture = SDL_CreateTextureFromSurface(renderer, image);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
 
-
-
+        //texte
         police = TTF_OpenFont("../bin/fonts/arial.ttf", 65);
         SDL_Color colorBlack = {0, 0, 0};
         texte = TTF_RenderText_Blended(police, "mon texte", colorBlack);
