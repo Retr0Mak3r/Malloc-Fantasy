7071136bea396eb356e0eb2821e6887dcb5ce245
Retr0 <ngallot@myges.fr>
2021-02-24 23:08:04 +0100

    harmonisation

--- Sources/battle.c
+++ Sources/battle.c
@@ -1,6 +1,11 @@
 #include "../Headers/prototype.h"
 
 void battle(){
+    char *background_battle;
+    background_battle = "../bin/graphics/map_battle.bmp";
+    setBackground(background_battle);
+   // SDL_Color color = {125, 125, 125};
+   // addText(60,color,"AH ! un ennemi attaque ! Que voulez-vous faire ? ",500,500);
     player_stat player;
     ennemy_stat ennemy;
     int player_action = 0;
@@ -17,21 +22,23 @@ int player_turn(int ennemy_choice){
     int mana = 0;
     SDL_Color color = {255, 255, 255};
     player_stat player;
-    ennemy_stat *ennemy;
-   addText(15,color,"Que voulez-vous faire ? ",500,990);
+    ennemy_stat *ennemy = malloc(sizeof(ennemy));
+//    addText(15,color,"Que voulez-vous faire ? ",500,990);
+    printf("\nQue voulez-vous faire ? ");
     scanf("%d",&action);
     switch (action) {
         case 1:
+            printf("\nVous avez attaqué");
             if (ennemy_choice == 2){
                 temp = player.attack - ennemy->defense;
                 ennemy->life -= temp;
             }
             break;
         case 2:
-         //printf();
+         printf("\nVous avez défendu");
          break;
         case 3:
-            //printf(vous avez utilise de la magie);
+            printf("\nvous avez utilise de la magie");
             if (ennemy_choice == 2 ){
                 temp = player.magic - ennemy->defense;
                 ennemy->life -= temp;
@@ -48,7 +55,7 @@ int  ennemy_turn(int player_choice){
     player_stat *player = malloc(sizeof(player));
     ennemy_stat ennemy;
     if (action == 1){
-        //l'ennemi attaque
+        printf("l\' ennemi attaque");
         if (player_choice == 2){
             temp = ennemy.attack - player->defense;
             player->life -= temp;
