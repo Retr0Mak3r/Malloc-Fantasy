85075f65f14b1c25206454611b89ee6ce804b64e
mikalia <mika-xuan@hotmail.com>
2021-02-24 03:32:50 +0100

    start ok menu ok sql ok la debut creation du perso

--- Headers/prototype.h
+++ Headers/prototype.h
@@ -13,7 +13,7 @@ extern void drawGame(int);
 extern int createPlayer();
 extern void setBackground(char*);
 extern void addText(int size, SDL_Color color, char *text, int x, int y);
-extern void fMysql(void);
+
 
 
 
